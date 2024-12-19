5ebe5e4b63fad4aba59f811d9cd3c72cb89295a0
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-12-19 18:58:22 +0530

    updated settings

--- vehicle_tracking/vehicle_tracking/settings.py
+++ vehicle_tracking/vehicle_tracking/settings.py
@@ -31,13 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom app
+    'tracker',
+    'rest_framework',
+    'channels',  # For WebSockets
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
