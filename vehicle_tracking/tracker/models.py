bcd831f4ace41e28f97f82c1142ac5442c39c6da
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-12-20 19:43:43 +0530

    Updated models

--- vehicle_tracking/tracker/models.py
+++ vehicle_tracking/tracker/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=50)
+    current_latitude = models.FloatField()
+    current_longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+class Journey(models.Model):
+    vehicle_1 = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='journeys_as_vehicle_1')
+    vehicle_2 = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='journeys_as_vehicle_2')
+    destination_latitude = models.FloatField()
+    destination_longitude = models.FloatField()
+    start_time = models.DateTimeField(auto_now_add=True)
+
