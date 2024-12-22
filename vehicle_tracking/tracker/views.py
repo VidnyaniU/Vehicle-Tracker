a6cc04413af133ab6cb8991974bc0c7fcfea6052
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-12-22 12:05:14 +0530

    Updated views

--- vehicle_tracking/tracker/views.py
+++ vehicle_tracking/tracker/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Vehicle, Journey
+from .serializers import VehicleSerializer, JourneySerializer
 
-# Create your views here.
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = Journey.objects.all()
+    serializer_class = JourneySerializer
+
