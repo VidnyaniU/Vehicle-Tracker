4f23e679ed755705f7d8d3186888c06765bf379f
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-12-27 19:24:46 +0530

    updated views.py

--- vehicle_tracking/tracker/views.py
+++ vehicle_tracking/tracker/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from .models import Vehicle, Journey
 from .serializers import VehicleSerializer, JourneySerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .services import calculate_route
 
 class VehicleViewSet(viewsets.ModelViewSet):
     queryset = Vehicle.objects.all()
@@ -10,3 +13,13 @@ class JourneyViewSet(viewsets.ModelViewSet):
     queryset = Journey.objects.all()
     serializer_class = JourneySerializer
 
+
+@api_view(['POST'])
+def get_route(request):
+    origin = request.data.get('origin')
+    destination = request.data.get('destination')
+    if not origin or not destination:
+        return Response({"error": "Origin and destination are required."}, status=400)
+
+    route = calculate_route(origin, destination)
+    return Response(route)
