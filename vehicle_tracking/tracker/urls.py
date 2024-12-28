e9a9123c1beed84d61f2e780a5a09bcd78b2429e
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-12-28 17:17:46 +0530

    updated urls.py

--- vehicle_tracking/tracker/urls.py
+++ vehicle_tracking/tracker/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import VehicleViewSet, JourneyViewSet
+from .views import VehicleViewSet, JourneyViewSet,get_route
 
 router = DefaultRouter()
 router.register(r'vehicles', VehicleViewSet)
 router.register(r'journeys', JourneyViewSet)
 
+
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('route/', get_route),
 ]
