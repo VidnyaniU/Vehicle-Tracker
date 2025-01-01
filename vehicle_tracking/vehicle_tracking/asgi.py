8be584831b5450f15f9c2325e61642c8b5685f71
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2025-01-01 12:50:15 +0530

    updated asgi.py

--- vehicle_tracking/vehicle_tracking/asgi.py
+++ vehicle_tracking/vehicle_tracking/asgi.py
@@ -11,6 +11,19 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicle_tracking.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import tracker.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicle_tracking.settings')
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            tracker.routing.websocket_urlpatterns
+        )
+    ),
+})
